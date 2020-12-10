7228ec5a9e024d466e2c6c26c4c329896f51326e
Leifer Mendez <leifer33@gmail.com>
2020-12-10 20:57:08 +0100

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'example-react-form';
+  public formLogin: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ],
+      terms: ['', [
+        Validators.required,
+        Validators.requiredTrue
+      ]]
+    });
+
+    this.loadAPI();
+  }
+
+  loadAPI(): any {
+    const response = {
+      email: 'leifer33@gmail.com',
+      password: '12345678',
+      terms: true
+    };
+
+
+    this.formLogin.patchValue(response);
+
+  }
+
+  send(): any {
+    console.log(this.formLogin.value);
+  }
 }
